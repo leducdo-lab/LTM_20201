c18dcb377f5c8f7c75e288696c214d062d6d1a5d
HoKhanhDuong <10dragonball.5onepice.1naruto@gmail.com>
2020-12-20 08:41:07 +0700

    ko biet

---
+++ Checkers - Server/bin/SQL/SQLQuery1.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE Checker
+USE Checker
+GO
+CREATE TABLE Users(
+	UserID INT IDENTITY(1,1),
+	Name VARCHAR(30),
+	Pass VARCHAR(30),
+	Score INT DEFAULT 0,
+	PRIMARY KEY (UserID)
+);
+INSERT INTO Users VALUES('DuongNeb','KongfuPanDa',10);
+
+CREATE TABLE Room(
+	RoomID INT IDENTITY(1,1),
+	RMaster INT,
+	UserID INT,
+	PRIMARY KEY (RoomID),
+	FOREIGN KEY (RMaster) REFERENCES Users(UserID),
+	FOREIGN KEY (UserID) REFERENCES Users(UserID)
+)
